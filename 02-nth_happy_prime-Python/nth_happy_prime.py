dea86079a9edcdc5be9525c331728f02e8393d19
sameer shaik <63531483+sameer-shaik-65@users.noreply.github.com>
2021-08-18 23:30:25 +0530

    sssww

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -3,6 +3,22 @@
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
 
-
-def fun_nth_happy_prime(n):
-	return 0
+def Happynum(n):
+    sum = 0
+    while(n!=0):
+        sum += (n%10)**2
+        n//=10
+    if sum == 1:
+        return True
+    elif sum<10:
+        return False
+    else:
+        return Happynum(sum)
+def nth_happy_number(n):
+    f = 1
+    g = 0
+    while(f<=abs(n)):
+        g+=1
+        if(Happynum(g)):
+            f+=1
+    return g
