b54f80225bb56f3252771010f4d7ea33e8a0eb1a
sameer shaik <63531483+sameer-shaik-65@users.noreply.github.com>
2021-08-25 10:59:31 +0530

    rrttt

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -7,5 +7,32 @@
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
 # returns 2
 
-def fun_nth_palindromic_prime(n):
-	return 0
+def isPrime(n):
+    if n>1:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+        return True
+def revNumber(n):
+    p=n
+    rn = 0
+    while(n!=0):
+        rm = n%10
+        rn = rn*10 + rm
+        n = n//10
+    return (rn==p)
+
+def isPalindromePrime(n):
+    if (isPrime(n)==True) and (revNumber(n)==True):
+        return True
+    return False
+
+
+def nthpalidrome(n):
+    num=10
+    guess=0
+    while(n<=guess):
+        if isPalindromePrime(n):
+            guess+=1
+        num+=1
+    return num
