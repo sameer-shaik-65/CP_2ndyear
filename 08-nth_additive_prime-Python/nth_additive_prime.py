c87112bb1d9de82c639dd6e1063a3225d1e5f329
sameer shaik <63531483+sameer-shaik-65@users.noreply.github.com>
2021-08-25 10:52:45 +0530

    ctt

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,5 +6,25 @@
 
 
 
-def fun_nth_additive_prime(n):
-	return 1
+def isPrime(n):
+    n = int(n)
+    if n>1:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+        return True
+ 
+def getSum(n):
+ 
+    sum = 0
+    while (n != 0):
+        sum = sum + n % 10
+        n = n / 10
+    return sum
+
+def isAdditivePrime(n):
+    if (not isPrime(n)):
+        return False
+    return isPrime(getSum(n))
+    
+print(isAdditivePrime(290))
